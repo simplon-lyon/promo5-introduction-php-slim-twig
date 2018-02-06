337183c8726a74296e8303dff932a67c15faf390
matmorel <pub.imagiin@gmail.com>
2018-02-06 10:11:45 +0100

    exercice correction

--- default/src/routes.php
+++ default/src/routes.php
@@ -5,6 +5,18 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->map(['GET', 'POST'], '/login', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+    $body = $request->getParsedBody();
+    $body['isLogged'] = ($body['email'] === 'toto@gmail.com' && $body['password'] === 'pass');
+
+    // Render index view
+    return $this->view->render($response, 'login.twig', [
+        'body' => $body
+    ]);
+})->setName('login');
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
